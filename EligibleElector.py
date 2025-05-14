58d1e78d135daf311d1f295d0a013d29ad0944cc
gamroyan <ggamroyan@gmail.com>
2025-05-13 23:16:15 -0700

    finished eligible elector

---
+++ EligibleElector.py
@@ -0,0 +1,15 @@
+def EligibleElector():
+    age = int(input("How old are you? "))
+
+    if age < 0:
+        print("Don't lie!")
+    elif age > 70:
+        print("Uhh you haven't been voting this whole time? You're eligible!")
+    elif age >= 18:
+        print("Congratulations! You are eligible to vote. Go make a difference!")
+    else:
+        yrs = 18 - age
+        print("Oops! You’re not eligible yet. But hey, only " + str(yrs) + " more years to go!")
+    return
+
+EligibleElector()
