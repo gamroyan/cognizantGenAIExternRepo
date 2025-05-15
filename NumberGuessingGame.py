01a0b9a0955df54fd1799423836d4e2f0a115f3a
gamroyan <ggamroyan@gmail.com>
2025-05-14 19:17:05 -0700

    finished assignment

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,1 +1,32 @@
 # Project: Number Guessing Game
+import random
+
+def numberGuessingGame():
+    number_to_guess = random.randint(1, 100)
+    number_of_tries = 0
+
+    while True:
+        num = int(input("Guess the number (between 1 and 100): "))
+
+        # number 
+        if num > number_to_guess:
+            number_of_tries += 1
+            if number_of_tries == 10:
+                print("Game over! Better luck next time!")
+                break
+            print("Too high! Try again.")
+
+        elif num < number_to_guess:
+            number_of_tries += 1
+            if number_of_tries == 10:
+                print("Game over! Better luck next time!")
+                break
+            print("Too low! Try again.")
+        else:
+            number_of_tries += 1
+            print("Congratulations! You guessed it in " + str(number_of_tries) + " attempts!")
+            break
+
+    return
+
+numberGuessingGame()
