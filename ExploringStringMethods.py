6d5664cf686401489f8ee47fe6ae54f5773e128e
gamroyan <ggamroyan@gmail.com>
2025-05-19 14:26:51 -0700

    finished assignment

---
+++ ExploringStringMethods.py
@@ -0,0 +1,8 @@
+# Assignment: Exploring String Methods
+# note that you'd need to comment out each task one at a time for
+# them to work as needed
+
+# Task 1 - String Slicing and Indexing
+def stringSlicingAndIndexing():
+    
+    return
